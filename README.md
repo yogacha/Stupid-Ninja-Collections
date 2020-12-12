ed2e04d9dee2b200f75b8779439f92126e9f93b9
yoga <googagagaga@gmail.com>
2020-12-12 13:22:46 +0800

    0

--- README.md
+++ README.md
@@ -1,12 +1,17 @@
 # Stupid-Ninja-Collections
 
 Prototypes of random ideas pop into my head. Usable but not stable. 
+
 See example code in `usage.ipynb`.
 
 
 Content:
 
-+ pandas extension:
-  Book, Dict[DataFrame or Series] 
-    A class mapping pandas-like operations to every pandas objects in the dictionary.
-    It works as if it's a pandas object.
++ ## pandas extension:
+
+  + ### ***Book***, Dict[DataFrame or Series] 
+      **To get rid of for loops and increase readablility when applying same opreation on different Dataframes.**
+
+      A class mapping pandas-like operations to every pandas objects in the dictionary.
+      
+      It works as if it's a pandas object.
---
+++ pandas_extension.py
@@ -0,0 +1,77 @@
+# %%
+"""
+Book:
+    A dictionary of pandas.DataFrame,
+    mapping pandas.DataFrame method to every DataFrame in dict
+
+"""
+import pandas as pd
+
+
+Function = type(lambda:0)
+
+
+class _Loc(object):
+    """
+    For pandas-like slicing: loc[...], iloc[...] 
+    """
+
+    def __init__(self, caller, is_iloc=False):
+        self.caller = caller
+        self.is_iloc = is_iloc
+
+    def __getitem__(self, slicing):
+        if self.is_iloc:
+            return Book({
+                name: df.iloc.__getitem__(slicing)
+                 for name, df in self.caller.items()
+            })
+        else:
+            return Book({
+                name: df.loc.__getitem__(slicing)
+                 for name, df in self.caller.items()
+            })
+
+
+def method_mapping(method):
+
+    def method_mapper(self, *args, **kwargs):
+        return Book({
+            name: method(df_or_srs, *args, **kwargs)
+                for name, df_or_srs in self.items()
+        })
+
+    return method_mapper
+
+
+
+class Book(dict):
+
+    """
+    Collection of pandas.DataFrame or pnadas.Series
+    """
+    
+    for method_name in dir(pd.DataFrame):
+        method = getattr(pd.DataFrame, method_name)
+
+        if ( isinstance( method, property ) 
+             or method_name.startswith('_') 
+             or method_name in dir(dict) ):
+            pass
+
+        elif isinstance(method, Function):
+            locals()[method_name] = method_mapping(method)
+
+    @property
+    def loc(self):
+        return _Loc(self, is_iloc=False)
+
+    @property
+    def iloc(self):
+        return _Loc(self, is_iloc=True)
+
+    @property
+    def T(self):
+        return Book({
+            name: df.T for name, df in self.items()
+        })
