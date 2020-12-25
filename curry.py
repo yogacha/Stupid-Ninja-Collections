ba6f459b405f4ebf4a964ebd56b04bcc4678f2f5
yoga <googagagaga@gmail.com>
2020-12-25 11:42:53 +0800

    magic curry

--- curry.py
+++ curry.py
@@ -1,5 +1,5 @@
 from typing import Callable
-from functools import partial, update_wrapper
+import functools
 
 
 def args_post_application(wrapped: Callable):    
@@ -12,13 +12,24 @@ def args_post_application(wrapped: Callable):
         kwargs_acceptor(x=4, y=5, z=6)(1, 2, 3)
     """
     def kwargs_acceptor(**kwargs):
-        wrapper = partial(wrapped, **kwargs)
+        wrapper = functools.partial(wrapped, **kwargs)
         wrapper.__name__ = wrapped.__name__
         return wrapper
     
-    update_wrapper(kwargs_acceptor, wrapped)
+    functools.update_wrapper(kwargs_acceptor, wrapped)
     kwargs_acceptor.__doc__ = """NOTE: Post applied args, use kwargs only.\n\n"""
     if wrapped.__doc__:
         kwargs_acceptor.__doc__ += wrapped.__doc__
 
     return kwargs_acceptor
+
+
+def update_wrapper(wrapper, wrapped, *args, **kwargs):
+    """
+    Extended version of `functools.update_wrapper` that pass `__kwdefaults__`
+    """
+    res = functools.update_wrapper(wrapper, wrapped, *args, **kwargs)
+    wrapper.__kwdefaults__ = wrapped.__kwdefaults__
+    return res
+
+functools.update_wrapper(update_wrapper,  functools.update_wrapper)  # inherit doc string
